f00141a01a72a19aa00e5b7dc4ba6ebbed1ed816
Juan Edi <juan@noredink.com>
2022-06-03 09:44:03 -0300

    rename pkgs-darwin -> pkgs-intel

--- shell.nix
+++ shell.nix
@@ -2,19 +2,19 @@ let
   sources = import ./nix/sources.nix;
   niv = import sources.niv { };
   pkgs = import sources.nixpkgs { };
-  pkgs-darwin = import sources.nixpkgs { localSystem = "x86_64-darwin"; };
+  pkgs-intel = import sources.nixpkgs { localSystem = "x86_64-darwin"; };
 in
 pkgs.mkShell {
   buildInputs = [
     niv.niv
 
-    pkgs-darwin.cabal-install
-    pkgs-darwin.elmPackages.elm
-    pkgs-darwin.elmPackages.elm-format
-    pkgs-darwin.elmPackages.elm-language-server
-    pkgs-darwin.ghc
-    pkgs-darwin.haskell-language-server
-    pkgs-darwin.haskellPackages.fourmolu
-    pkgs-darwin.zlib
+    pkgs-intel.cabal-install
+    pkgs-intel.elmPackages.elm
+    pkgs-intel.elmPackages.elm-format
+    pkgs-intel.elmPackages.elm-language-server
+    pkgs-intel.ghc
+    pkgs-intel.haskell-language-server
+    pkgs-intel.haskellPackages.fourmolu
+    pkgs-intel.zlib
   ];
 }
